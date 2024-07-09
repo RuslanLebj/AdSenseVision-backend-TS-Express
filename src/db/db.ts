0cb828e129e1046bad8554c590c7a390e3cab8f9
Ruslan <dressgecko@gmail.com>
2024-07-09 23:06:42 +0500

    fix some lol, export default work with db

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,5 +2,5 @@ const knex = require("knex")
 const knexfile = require("./knexfile")
 
 const db = knex(knexfile.development)
-// typescript export default is not supported by knex cli by default.
-module.exports = db
+
+export default db;
